1f3460262c272b693e4373e56386fc5482a87ca0
dinesh <dineshvhingwe@gmail.com>
2019-01-22 10:54:24 +0530

    committed to local

--- src/main/java/com/springBoot/GreetClass.java
+++ src/main/java/com/springBoot/GreetClass.java
@@ -1,6 +1,7 @@
 package com.springBoot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class GreetClass
 	public String greetSimple()
 	{
 		return "Hello User. This is First Spring boot REST WS!!";
+	}
+	@GetMapping(value="/simple",produces="application/xml")  ///note it @GetMapping  similar to RequestMapping
+	public String greetwithName(@PathVariable("name") String name)
+	{
+		return "Hello "+name+". This is Spring boot REST WS!!";
 	}
 
 }
